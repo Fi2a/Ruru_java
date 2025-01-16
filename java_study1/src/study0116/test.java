428b7c052c649e6a2050b39db15170a3bff937c7
Fi2a <fia_to@naver.com>
2025-01-16 12:49:59 +0900

    0116

--- java_study1/src/study0116/test.java
+++ java_study1/src/study0116/test.java
@@ -4,7 +4,26 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/*
+		 *   <- /*엔터
+		 */
+		System.out.println("자바 이클립스 테스트");
+		
+//		데이터를 사용하려면 변수를 선언 해야 한다.
+//		변수를 선언 할때는 반드시 데이터 타입으 정해야한다.
+//		변수의 이름은 저장할 데이터에 맞는 의미 있는 이름으로 짓기
+/*		정수 : int, byte, short, long
+ * 		실수 : float, double
+ * 		문자 : char
+ * 		논리 : boolean
+ */
+		int age; 
+		age = 100;	// age 변수에 100저장 (대입연산)
+		
+		age = 27;	// 기존 100 값을 27로 
+		
+		age = '가'; 	// 자바에서 '' 표현은 문자를 의미
+					// "" 표현은 문자열을 의미
 	}
 
-}
+} 
