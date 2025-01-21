7b137851b3546c082fcb72f316c0de7958dd90a2
Fi2a <fia_to@naver.com>
2025-01-21 09:05:56 +0900

    project

---
+++ java_study1/src/project/project0120.java
@@ -0,0 +1,31 @@
+package project;
+
+public class project0120 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		int a = 20; // (apple in a box)
+		int o = 15; // (orange in a box)
+		
+		int ab = 11; // (appleboxes)
+		int ob = 12; // (oragneboxes)
+		
+		int ta = a*ab; // (total apples)
+		int to = o*ob; // (total oranges)
+		
+		int tf = ta+to; // (total fruits)
+		
+		System.out.println("총 과일 갯수 : "+ tf);
+		
+			
+		
+
+	}
+		
+	
+			
+
+}
